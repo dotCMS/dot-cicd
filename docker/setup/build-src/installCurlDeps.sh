88d5117601fdc3d237960db27db33d670ade74ce
Victor Alfaro <victor.alfaro@dotcms.com>
2020-11-24 19:35:35 -0600

    newman test

--- docker/setup/build-src/installCurlDeps.sh
+++ docker/setup/build-src/installCurlDeps.sh
@@ -5,4 +5,4 @@ set -e
 npm install -g newman
 npm uninstall -g har-validator
 npm install -g newman-reporter-htmlextra
-npm install -g highlight.js@10 --save-dev
+#npm install -g highlight.js@10 --save-dev
