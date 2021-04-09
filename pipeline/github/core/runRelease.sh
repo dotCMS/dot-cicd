8e382b4e3610d895f710c21e9e62a70d3e5eef8f
Daniel Silva <daniel.silva@dotcms.com>
2021-04-09 15:47:45 -0500

    Surrounded by quotes

--- pipeline/github/core/runRelease.sh
+++ pipeline/github/core/runRelease.sh
@@ -23,7 +23,7 @@ echo "Executing: docker run --rm \
   -e docker_password=${DOCKER_PASSWORD} \
   -e is_release=${IS_RELEASE} \
   -e debug=${DEBUG} \
-  -e ee_rsa=${SSH_RSA_KEY} \
+  -e ee_rsa=\"${SSH_RSA_KEY}\" \
  ${IMAGE_NAME} $2 $3 $4 $5 $6"
 echo '############################################################################################################################################'
 
