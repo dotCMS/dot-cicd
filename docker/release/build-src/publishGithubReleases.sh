57e714c86f4c84866eac86d405c4a48ec9bb1d7d
Daniel Silva <daniel.silva@dotcms.com>
2021-04-09 16:49:23 -0500

    alter tag name to get branch

--- docker/release/build-src/publishGithubReleases.sh
+++ docker/release/build-src/publishGithubReleases.sh
@@ -8,9 +8,13 @@ if [[ ${is_release} != true ]]; then
   exit 1;
 fi
 
+RELEASE_PREFIX="release-"
+RELEASE_BRANCH_NAME=${ee_rsa/v/$RELEASE_PREFIX}
+
 echo
 echo '######################################################################'
 echo 'Releasing on enterprise'
+echo "RELEASE_BRANCH_NAME: " + ${RELEASE_BRANCH_NAME}
 echo '######################################################################'
 
 mkdir -p ~/.ssh
@@ -24,7 +28,7 @@ git config --global pull.rebase false
 
 git clone git@github.com:dotCMS/enterprise.git
 cd enterprise
-git checkout ${ee_build_id}
+git checkout ${RELEASE_BRANCH_NAME}
 git fetch --all
 git commit --allow-empty -m "Publish Release"
 
