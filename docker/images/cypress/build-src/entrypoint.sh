daa72ab401dafbc9650bf4e273a4233c88733b54
Victor <victor.alfaro@dotcms.com>
2021-06-03 15:43:21 -0600

    Chaina squash me

--- docker/images/cypress/build-src/entrypoint.sh
+++ docker/images/cypress/build-src/entrypoint.sh
@@ -51,10 +51,11 @@ pwd
 ls -la
 npm run build:prod
 npm install -g nx
+BASEURL=http://dotcms-app:8080 npm run e2e
 # npm run e2e
 # ng e2e dotcms-ui-e2e --headless true --base-url ${BASEURL:-http://localhost:8080} || (npm run poste2e && exit 1)
-nx e2e dotcms-ui-e2e --headless true --base-url http://dotcms-app:8080
-npm run poste2e
+#nx e2e dotcms-ui-e2e --headless true --base-url http://dotcms-app:8080
+#npm run poste2e
 # cypress ....
 
 # publicar los tests
