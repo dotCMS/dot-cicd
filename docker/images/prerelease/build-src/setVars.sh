35cfa8c78e7be67f30395679d7d89fdf3a489262
Victor Alfaro <victor.alfaro@dotcms.com>
2022-01-13 09:58:06 -0600

    test

--- docker/images/prerelease/build-src/setVars.sh
+++ docker/images/prerelease/build-src/setVars.sh
@@ -4,6 +4,8 @@
 # Script: setVars.sh
 # Set important env-vars to be used across the release process
 
+uname -m; uname -r
+
 : ${FROM_BRANCH:="master"} && export FROM_BRANCH
 export ENTERPRISE_DIR=dotCMS/src/main/enterprise
 
@@ -12,4 +14,3 @@ master_branch=master
 [[ "${DRY_RUN}" == 'true' ]] && master_branch="${master_branch}-cicd"
 export master_branch
 export GIT_CLONE_STRATEGY=full
-
