9516652aa50a46fec8c4333ce97acaeeb29da284
Victor Alfaro <victor.alfaro@dotcms.com>
2023-03-30 11:21:29 -0600

    Using other path to get dotcms jar when uploading jars

--- docker/images/release/build-src/generateAndUploadJars.sh
+++ docker/images/release/build-src/generateAndUploadJars.sh
@@ -19,13 +19,35 @@ if [[ "${is_release}" == 'true' ]]; then
   executeCmd "./gradlew clean createDistPrep"
   popd
 
+  #dotcms_lib_dir=dist/dotserver/tomcat-9.0.60/webapps/ROOT/WEB-INF/lib
+  dotcms_lib_dir=dotCMS/build/libs
+  dotcms_jar=dotcms_${RELEASE_VERSION}
+  dotcms_jar_path=${dotcms_lib_dir}/${dotcms_jar}
+  executeCmd "ls -las ${dotcms_lib_dir}/dotcms_*.jar"
+  executeCmd "mv ${dotcms_jar_path}_999999.jar ${dotcms_jar_path}.jar"
+  executeCmd "ls -las ${dotcms_lib_dir}/dotcms_*.jar"
+
+  pushd dotCMS
+  echo
+  echo '####################'
+  echo 'Uploading DotCMS jar'
+  echo '####################'
+  executeCmd "gradle -b deploy.gradle uploadArchives
+    ${releaseParam}
+    -PgroupId=com.dotcms
+    -Pusername=${repo_username}
+    -Ppassword=${repo_password}
+    -Pfile=../${dotcms_jar_path}.jar"
+  [[ ${cmdResult} != 0 ]] && exit 1
+  popd
+
   pushd ${ENTERPRISE_DIR}
   enterprise_lib_dir=build/libs
   ee_jar=${enterprise_lib_dir}/ee_${RELEASE_VERSION}.jar
   executeCmd "./gradlew clean jar"
-  executeCmd "ls -las ${enterprise_lib_dir}"
+  executeCmd "ls -las ${enterprise_lib_dir}/ee_*.jar"
   executeCmd "mv ${enterprise_lib_dir}/ee_obfuscated.jar ${ee_jar}"
-  executeCmd "ls -las ${enterprise_lib_dir}"
+  executeCmd "ls -las ${enterprise_lib_dir}/ee_*.jar"
   popd
 
   pushd dotCMS
@@ -41,27 +63,5 @@ if [[ "${is_release}" == 'true' ]]; then
     -Ppassword=${repo_password}
     -Pfile=./src/main/enterprise/${ee_jar}"
   [[ ${cmdResult} != 0 ]] && exit 1
-  popd
-
-  dotcms_lib_dir=dist/dotserver/tomcat-9.0.60/webapps/ROOT/WEB-INF/lib
-  dotcms_jar=dotcms_${RELEASE_VERSION}
-  dotcms_jar_path=${dotcms_lib_dir}/${dotcms_jar}
-  executeCmd "ls -las ${dotcms_lib_dir}"
-  executeCmd "mv ${dotcms_jar_path}_999999.jar ${dotcms_jar_path}.jar"
-  executeCmd "ls -las ${dotcms_lib_dir}"
-
-  pushd dotCMS
-  echo
-  echo '####################'
-  echo 'Uploading DotCMS jar'
-  echo '####################'
-  executeCmd "gradle -b deploy.gradle uploadArchives
-    ${releaseParam}
-    -PgroupId=com.dotcms
-    -Pusername=${repo_username}
-    -Ppassword=${repo_password}
-    -Pfile=../${dotcms_jar_path}.jar"
-  [[ ${cmdResult} != 0 ]] && exit 1
-
   popd
 fi
