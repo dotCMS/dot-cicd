ae1f8de44132030cad88a876fc58dc943546c2ac
Victor Alfaro <victor.alfaro@dotcms.com>
2023-06-14 15:40:17 -0600

    Fixing error

--- docker/images/release/build-src/generateAndUploadJars.sh
+++ docker/images/release/build-src/generateAndUploadJars.sh
@@ -25,12 +25,12 @@ dotcms_jar_path=${dotcms_lib_dir}/${dotcms_jar}
 github_sha=$(git rev-parse HEAD)
  [[ "${is_release}" == 'true' ]] && releaseParam='-Prelease=true'
 executeCmd "ls -las ${dotcms_lib_dir}/dotcms_*.jar"
-if [[ -f ${dotcms_jar_path}_null.jar ]]; then
-  suffix=null
+if [[ -f ${dotcms_lib_dir}/dotcms_null.jar ]]; then
+  jar_file=${dotcms_lib_dir}/dotcms_null.jar
 else
-  suffix=${github_sha::7}
+  jar_file=${dotcms_jar_path}_${github_sha::7}.jar
 fi
-executeCmd "mv ${dotcms_jar_path}_${suffix}.jar ${dotcms_jar_path}.jar"
+executeCmd "mv ${jar_file} ${dotcms_jar_path}.jar"
 executeCmd "ls -las ${dotcms_lib_dir}/dotcms_*.jar"
 
 if [[ "${is_release}" == 'true' ]]; then
